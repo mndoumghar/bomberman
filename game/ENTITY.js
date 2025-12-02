770ffee98d30b8eb169af40a117ab0f1010519ee
mndoumghar <mohamedndoumghar@gmail.com>
2025-12-02 04:05:27 +0100

    first commit -m bomberman

---
+++ game/ENTITY.js
@@ -0,0 +1,36 @@
+export default class Entity {
+    constructor({ tag = "div", className = "" } = {}) {
+        this.el = document.createElement(tag);
+
+        const gameContainer = document.getElementById('container');
+        gameContainer.appendChild(this.el);
+
+        this.el.className = 'entity ' + className;
+
+        this.x = 0;
+        this.y = 0;
+    }
+
+    setX(x) {
+        this.x = x;
+        this.updatePosition();
+    }
+
+    setY(y) {
+        this.y = y;
+        this.updatePosition();
+    }
+
+    updatePosition() {
+        if (this.el) {
+            this.el.style.transform = `translate(${this.x}px, ${this.y}px)`;
+        }
+    }
+
+    remove() {
+        if (this.el) {
+            this.el.remove();
+            this.el = null;
+        }
+    }
+}
